cb9a1a87837c0af788a51a538522b534625a5a77
Famous Clark <clark.famous@gmail.com>
2018-12-09 06:53:04 -0500

    Update twitter.js

--- routes/api/twitter.js
+++ routes/api/twitter.js
@@ -2,12 +2,14 @@ const Twitter = require('twitter');
 const getBearerToken = require('get-twitter-bearer-token');
 
 module.exports = () => {
+  /*
   let twitter = new Twitter({
     consumer_key: '2Jz7kiKdPuGYAg1f7k9Rhh5de',
     consumer_secret: 'QWX73rxWYqTg9IOzJxvZG7vFVXVgEq7PqAqiwErIpgQ7Zv6XlT',
     access_token_key: '1071717626345340933-70jyDSeNONjOvOcWaNOlTz8zrbXM51',
     access_token_secret: 'haX1GSHAU9D8Vpep1zrDsagCSo0fASNpxoc2rgYfpLQiA'
   });
+  */
   
   app.get('/twitter', (req, res) =>{
     const key: '2Jz7kiKdPuGYAg1f7k9Rhh5de';
