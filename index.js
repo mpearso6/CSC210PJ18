5f7e7f56bb77d63ca06737b1441f865e7270a1da
Famous Clark <clark.famous@gmail.com>
2018-12-09 19:23:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const twitter = require('./routes/api/twitter');
+const twitter = require('./routes/api/twitter')(app, io);
 const user = require('./routes/user');
 const saved_tweets = require('./routes/saved_tweets');
 const standards = require('./routes/standards');
