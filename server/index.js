e8a1fb883a05d26fabc877a33641948f7b913af9
famousclark <clark.famous@gmail.com>
2018-12-15 12:16:49 -0500

    server change

--- server/index.js
+++ server/index.js
@@ -37,5 +37,5 @@ app
   .use('/watson_analysis', watson_analysis)
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('../client/public/indexs'))
   .listen(PORT, () => console.log(`Main app Listening on ${ PORT }`));
