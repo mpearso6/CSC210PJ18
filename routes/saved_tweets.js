7fc0bd7b77c487c7e9c839b32c053e34d23a2d81
juan <jpuerto96@hotmail.com>
2018-12-20 08:44:05 -0500

    testing for post

--- routes/saved_tweets.js
+++ routes/saved_tweets.js
@@ -30,7 +30,7 @@ router.post('/', function (req, res) {
         connection.query("INSERT INTO SavedTweets(UserID, TwitterParams, TweetsReturned) VALUES (" + connection.escape(UserID) + ",'" +
             connection.escape(TwitterParams) + "','" + connection.escape(TweetsReturned) + "')", function(error, results, fields){
             if(error){
-                res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+                res.send(JSON.stringify({"status": 500, "error": error + "These are the values that were sent: \n" + UserID, "response": null}));
                 //If there is error, we send the error in the error section with 500 status
             } else {
                 res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
