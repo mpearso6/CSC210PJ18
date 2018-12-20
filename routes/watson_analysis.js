9f729b615d3c3e4fc226a696e7e3b39a1251d906
juan <jpuerto96@hotmail.com>
2018-12-19 20:13:51 -0500

    Add POST endpoints to saved_tweets, user, and watson_analysis.

--- routes/watson_analysis.js
+++ routes/watson_analysis.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var parser = require('body-parser');
 
 const db = require('./../db');
 const connection = db.connection;
@@ -18,8 +19,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function (req, res) {
-    console.log(req);
-    res.send("received POST request");
+    var SavedTweetsID = req.body.SavedTweetsID;
+    var WatsonDocumentAnalysis = req.body.WatsonDocumentAnalysis;
+    var WatsonSentenceAnalysis = req.body.WatsonSentenceAnalysis;
+
+    if(SavedTweetsID === null || WatsonDocumentAnalysis === null || WatsonSentenceAnalysis === null){
+        res.send(JSON.stringify({"status": 500, "error": "Invalid body specified", "response": null}));
+    }
+    else{
+        connection.query("INSERT INTO WatsonAnalysis(SavedTweetsID, WatsonDocumentAnalysis, WatsonSentenceAnalysis) VALUES (" + connection.escape(SavedTweetsID) + ",'" +
+            connection.escape(WatsonDocumentAnalysis) + "','" + connection.escape(WatsonSentenceAnalysis) + "')", function(error, results, fields){
+            if(error){
+                res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+                //If there is error, we send the error in the error section with 500 status
+            } else {
+                res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+                //If there is no error, all is good and response is 200OK.
+            }
+        });
+    }
 });
 
 module.exports = router;
