793348651f96312f8f36b74766a793f36e4aadc3
famousclark <clark.famous@gmail.com>
2018-12-20 10:25:26 -0500

    index update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app
   .use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Content-Security-Policy", "font-src '*'");
+    res.Header("Content-Security-Policy", "font-src '*'");
     next();
   })
   .use(express.static(path.join(__dirname, 'client/build')))
